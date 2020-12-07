f7491de5626409140a4c9c27f72d2402aec1f1a4
ControlCV <1916168788@qq.com>
2020-12-07 10:19:10 +0800

    解决80端口访问失败

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,10 +5,11 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.Resource;
 
 /**
  * (Payment)表控制层
@@ -24,15 +25,26 @@ public class PaymentController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @PostMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(@RequestBody Payment payment){
-        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    @RequestMapping(value = "/consumer/payment/create",produces = "application/json;charset=UTF-8")
+    public CommonResult create(@RequestBody Payment payment){
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<Payment> formEntity = new HttpEntity<>(payment, headers);//requestbody只能取一次
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",formEntity,CommonResult.class);
     }
 
 
     @PostMapping("/consumer/get/{id}")
     public CommonResult<Payment> getById(@PathVariable("id") String id){
         return restTemplate.postForObject(PAYMENT_URL+"/payment/get/"+id,"",CommonResult.class);
+    }
+
+    @PostMapping("/consumer/payment/create1")
+    public CommonResult<Payment> getById1(@RequestBody Payment payment){
+        System.out.println(payment);
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
 
 }
