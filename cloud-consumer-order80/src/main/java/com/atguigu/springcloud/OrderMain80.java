d44d795dd5d55cd0ddf35bcd1b0ee3c6bd28a309
zhjw <1916168788@qq.com>
2020-12-10 17:26:37 +0800

    openfeign设置超时时间

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -12,8 +12,8 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
  * @author LENOVO
  */
 @SpringBootApplication
-@EnableEurekaClient
-@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
+//@EnableEurekaClient
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class);
